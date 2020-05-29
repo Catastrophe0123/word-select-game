f5cffbadfc5bb71d2d23c84b728f62e5f67098a5
Catastrophe0123 <vnavaneeth15@gmail.com>
2020-05-29 16:37:57 +0530

    Updates

--- precache-manifest.1998877906899b8aae86c924f5d70a29.js
+++ precache-manifest.1998877906899b8aae86c924f5d70a29.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b19147d981cbac23d60ad0bdff6b3cc",
+    "revision": "906d9ca6dc3189331c274e36712988f4",
     "url": "/word-select-game/index.html"
   },
   {
-    "revision": "6d9dafcd23c7bcaaa983",
+    "revision": "64cf74f6b49ea97ecee7",
     "url": "/word-select-game/static/css/main.637b113d.chunk.css"
   },
   {
-    "revision": "e4eb7430a3f2fda0fbd8",
-    "url": "/word-select-game/static/js/2.7a375b52.chunk.js"
+    "revision": "cfad00ee0b4bfec117fd",
+    "url": "/word-select-game/static/js/2.406d6ad7.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/word-select-game/static/js/2.7a375b52.chunk.js.LICENSE.txt"
+    "url": "/word-select-game/static/js/2.406d6ad7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6d9dafcd23c7bcaaa983",
-    "url": "/word-select-game/static/js/main.1db6b533.chunk.js"
+    "revision": "64cf74f6b49ea97ecee7",
+    "url": "/word-select-game/static/js/main.f38d1b5e.chunk.js"
   },
   {
     "revision": "33696f001f99aa78cad1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/word-select-game/precache-manifest.4ee1220751abf922f69bdb043907d7ae.js"
+  "/word-select-game/precache-manifest.1998877906899b8aae86c924f5d70a29.js"
 );
 
 self.addEventListener('message', (event) => {
